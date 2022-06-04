3901e24dbe1c55bd1e99334e5fe5ea20bf14733b
S Pranav <sreenathpranav1@gmail.com>
2022-06-05 01:19:35 +0530

    v1 complete

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date= require(__dirname + "/date.js");
+
 const app = express();
 
-let items=["Read books","Workout","Meditate"];
+const items=["Read books","Workout","Meditate"];
+const workItems=[];
 
 app.set("view engine", "ejs"); // for setting ejs
 app.use(bodyParser.urlencoded({extended: true}));  // for body parsing 
@@ -11,25 +14,31 @@ app.use(express.static("public"));
 //get server
 app.get("/", function (req, res) { 
 
-  let today = new Date();
-  let options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-  };
-  let day= today.toLocaleDateString("en-US",options);
+    let day =date.getDate();
 
-  res.render("list", { kindOfDay: day,newListItems: items });  //res.render for displaying a html edited file (list.ejs files)
+  res.render("list", { listTitle: day,newListItems: items });  //res.render for displaying a html edited file (list.ejs files)
 });
 
 
 // post server
 app.post("/",function(req,res){
     let item= req.body.newItem;  //get inputed file from from to server.
-     items.push(item);
-    res.redirect("/");
+
+    if(req.body.list == "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
+
+app.get("/work",function(req,res){
+res.render("list",{listTitle:"Work List", newListItems: workItems});
+});
+
+
 
 // listen in port 3000
 app.listen(3000, function () {
